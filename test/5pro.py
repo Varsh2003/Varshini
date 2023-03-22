dc6d1d345d91ba9a59f9e414725006f777c8315c
Varshini <s.nagavarshini2003@gmail.com>
2023-03-22 09:10:35 +0530

    my program

---
+++ test/5pro.py
@@ -0,0 +1,33 @@
+def num(a):
+    if (100>=a):
+        print("The number is between 0 to 100")
+
+    elif (100<a) and (1000>=a):
+         print("The number is between 100 to 1000")
+
+    elif (1000<=a) and (2000>=a):
+         print("The number is between 1000 to 2000")
+         
+
+num(int(input("Enter the number : ")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
