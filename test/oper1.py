8501ff0a925fde5527220f7e60429a3f8cdf1798
Varshini <s.nagavarshini2003@gmail.com>
2023-03-23 08:40:13 +0530

    my program

---
+++ test/oper1.py
@@ -0,0 +1,9 @@
+import oper
+print("Welcome to the calculater")
+print("My addition value is : "+str (oper.add(2,3)))
+print("My substraction value is : "+str(oper.sub(4,6)))
+print("My multiplication value is : "+str(oper.mult(6,9)))
+print("My division value is : "+str(round(oper.div(55,66),3)))
+a=float(input("Enter my remainder value of a : "))
+b=float(input("Enter my remainder value of b : "))
+print("my remainder value is : "+str(oper))
