b9e8ebba1a6091df89cef431aee8c5f7aaef61eb
Varshini <s.nagavarshini2003@gmail.com>
2023-03-30 08:51:49 +0530

    my program

---
+++ test/whi.py
@@ -0,0 +1,21 @@
+print("Welcome to the interested puzzle game !!")
+print("Start the game : Im the mental in my class who am I")
+
+count=0
+while count<4:
+    ans=input("Enter your ans : ")
+    if ans=="priya":
+        print("HAHA!! YOU WON")
+        exit()
+    else:
+        count+=1
+    if count==1:
+        print("clue 1 : I am wear red colour dress !")
+    elif count==2:
+        print("I am brownish in colour !")
+    elif count==3:
+        print("I am smart girl in my class!")
+if count==4:
+    print("Sorry!! you lose")
+
+       
