d48d659289bd051463398a05e2d80dd8f591cd00
Varshini <s.nagavarshini2003@gmail.com>
2023-04-19 08:50:30 +0530

    My program

--- varsh/main.py
+++ varsh/main.py
@@ -3,8 +3,8 @@ print("welcome Patient Details application !!")
  
 while True:
     print("You can perform the below operations : ")
-    print("1.Register")
-    choice=input("Enter your choice[1],[2] : ")
+    print("1.Register\n""2.View patient\n""3.Update patient\n""4.delete patient\n")
+    choice=input("Enter your choice[1],[2],[3],[4] : ")
 
     if choice=="1":
         print("Welcome to patient registeration form")
@@ -12,6 +12,12 @@ while True:
     elif choice=="2": 
         print("welcome to the patient view form ")  
         patient.view()
+    elif choice=="3":
+        print("welcome to the update section")
+        patient.update()  
+    elif choice=="4":
+        print("Welcome to delete section")
+        patient.delete()  
     else:
         print("invalid choice")
     option=input("Do you want to continue (ok/not) : ")
--- varsh/patient.py
+++ varsh/patient.py
@@ -25,10 +25,67 @@ def view():
     f=open("reg.json","r")
     data=json.load(f)
     f.close()    
-    heading=["name","age","pro"]
+    heading=["s.no","name","age","pro"]
     table=[]
+    s_no=1
     for patient in data["patient"]:
-      temp_list=[patient["name"],patient["age"],patient["pro"]]
+      temp_list=[s_no,patient["name"],patient["age"],patient["pro"]]
       table.append(temp_list)
+      s_no+=1
     print(tabulate(table,headers=heading))  
 
+def update():
+   view()
+   f=open("reg.json","r")
+   data=json.load(f)
+   f.close()
+   pat_choice=input("Enter the serial number of the patient you want to update : ")
+   print("you can update \n1.Name\n2.Age\n3.pro\nAll")
+   update_choice=input("Please enter your choice : ")
+   s_no=1
+   for patient in data["patient"]:
+      if str(s_no)==pat_choice:
+         if update_choice=="1":
+            name=input("Enter updated name : ")
+            patient["name"]=name
+            print("Student data updated successfully!!")
+         elif update_choice=="2":
+            age=input("Enter update age :")   
+            patient["age"]=age
+            print("Student data is updated successfully")
+         elif update_choice=="3":
+            pro=input("Enter update pro : ")
+            patient["pro"]=pro
+         elif update_choice=="All":
+             patient["name"]=("Enter update name : ")
+             patient["age"]=("Enter update age : ")
+             patient["pro"]=("Enter update pro : ")
+             print("patient data is updated successfully")
+         else:
+             print("Invalid choice!!!!!!!")         
+             break
+             
+      s_no+=1
+
+   f=open("reg.json","w")
+   json.dump(data,f,indent=3)
+   f.close()  
+
+def delete():
+   view()
+   f=open("reg.json","r")
+   data=json.load(f)
+   f.close()
+   pat_choice=input("Enter the serial number of the patient you want to delete : ")
+  
+   s_no=1
+   for patient in data["patient"]:   
+     if str(s_no)==pat_choice:       
+       data["patient"].remove(patient)
+       print("Patient data is deleted successfully")
+
+     s_no+=1
+
+   f=open("reg.json","w")
+   json.dump(data,f,indent=3)
+   f.close()       
